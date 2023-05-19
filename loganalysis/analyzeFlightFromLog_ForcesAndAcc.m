aaaa2d6d25011ade4fe96001ec2fdaf435d9b247
luk8426 <luk8426@thi.de>
2023-05-19 19:17:03 +0200

    Fixed MaxDataTips in Plots

--- loganalysis/analyzeFlightFromLog_ForcesAndAcc.m
+++ loganalysis/analyzeFlightFromLog_ForcesAndAcc.m
@@ -7,23 +7,43 @@ actuators_output = ulog_obj.readTopicMsgs("TopicNames","actuator_outputs").Topic
 veh_acc = ulog_obj.readTopicMsgs("TopicNames","vehicle_acceleration").TopicMessages{1, 1};
 ang_acc = ulog_obj.readTopicMsgs("TopicNames","vehicle_angular_acceleration").TopicMessages{1, 1};
 
-subplot(2, 1, 1)
+subplot(3, 1, 1)
 lin_plot = plot(veh_acc.timestamp_sample, veh_acc.xyz);
 title("Vehicle accleration")
 xlabel("Time")
-ylabel("Acceleration [m/s]")
-max_i = find(veh_acc.xyz == max(veh_acc.xyz));
-datatip(lin_plot(1), "DataIndex", max_i(1), "Location","northwest");
-datatip(lin_plot(2), "DataIndex", max_i(2), "Location","northwest");
+ylabel("Acceleration [m/s^2]")
+legend("x", "y", "z")
+max_i = 1:3;
+for i = 1:3
+    max_i(i) = find(abs(veh_acc.xyz(:,i)) == max(abs(veh_acc.xyz(:,i))));
+end
+datatip(lin_plot(1), "DataIndex", max_i(1), "Location","southeast");
+datatip(lin_plot(2), "DataIndex", max_i(2), "Location","southwest");
 datatip(lin_plot(3), "DataIndex", max_i(3), "Location","southwest");
 
-subplot(2, 1, 2)
+subplot(3, 1, 2)
 ang_plot = plot(ang_acc.timestamp_sample, ang_acc.xyz);
 title("Angular acceleration")
 xlabel("Time")
-ylabel("Angluar acceleration [rad/s]")
-max_i = find(abs(ang_acc.xyz) == max(abs(ang_acc.xyz)));
+ylabel("Angluar acceleration [rad/s^2]")
+legend("x", "y", "z")
+for i = 1:3
+    max_i(i) = find(abs(ang_acc.xyz(:,i)) == max(abs(ang_acc.xyz(:,i))));
+end
 datatip(ang_plot(1), "DataIndex", max_i(1), "Location","northwest");
 datatip(ang_plot(2), "DataIndex", max_i(2), "Location","northwest");
 datatip(ang_plot(3), "DataIndex", max_i(3), "Location","northwest");
 
+subplot(3, 1, 3)
+jerk = gradient(veh_acc.xyz);
+jerk_plot = plot(veh_acc.timestamp_sample, jerk);
+title("Jerk of lateral acceleration")
+xlabel("Time")
+ylabel("Jerk in m/s^3")
+legend("x", "y", "z");
+for i = 1:3
+    max_i(i) = find(abs(jerk(:,i)) == max(abs(jerk(:,i))));
+end
+datatip(jerk_plot(1), "DataIndex", max_i(1), "Location","northwest");
+datatip(jerk_plot(2), "DataIndex", max_i(2), "Location","northwest");
+datatip(jerk_plot(3), "DataIndex", max_i(3), "Location","southwest");
