f6e1cb1ace7684003987aa6a12db7c420cf36e49
luk8426 <luk8426@thi.de>
2023-05-21 12:48:18 +0200

    Added Labels to show interesting values

--- livetelemetrie/telemetry_gui.py
+++ livetelemetrie/telemetry_gui.py
@@ -13,6 +13,15 @@ class Application(tk.Frame):
         self.canvas = None
         self.ax3D = None
         self.ax2D = None
+        self.speed = {"x": [0], "y": [0], "z": [0]}
+        self.acc = {"x": [0], "y": [0], "z": [0]}
+        self.acc_ang = {"x": [0], "y": [0], "z": [0]}
+        self.cur_velo_label, self.cur_acc_label, self.cur_acc_ang_label, self.cur_acc_ang_max_label = tk.Label(), tk.Label(), tk.Label(), tk.Label()
+        self.cur_acc_ang = ''
+        self.cur_acc = ''
+        self.cur_velo = ''
+        self.cur_acc_ang = None
+        self.cur_acc_ang_max = None
         self.positions = {"x": [0], "y": [0], "z": [0]}
         self.createWidgets()
         self.updateThread = td.Thread(target=self.readMavLinkAndUpdateGui)
@@ -27,17 +36,48 @@ class Application(tk.Frame):
             self.positions["x"].append(random.randint(3, 20))
             self.positions["y"].append(random.randint(3, 20))
             self.positions["z"].append(random.randint(3, 20))
+
+            self.acc_ang["x"].append(random.randint(3, 20))
+            self.acc_ang["y"].append(random.randint(3, 20))
+            self.acc_ang["z"].append(random.randint(3, 20))
+            
+            # Add the new Positions to the Plot
             x = np.asarray(self.positions["x"])
             y = np.asarray(self.positions["y"])
             z = np.asarray(self.positions["z"])
+            
             # Debugging
             #print(self.positions)
+            
+            # Update content of the plots
+
             self.ax2D.clear()
             self.ax3D.clear()
             self.ax2D.plot(x, y)
             self.ax2D.set_ylabel('X')
             self.ax2D.set_xlabel('Y')
             self.ax3D.plot(x, y, z)
+
+            # Edit the text of the Labels
+            self.cur_velo = "\tVelocity: \nx:\t"+ str(self.speed['x'][-1]) + '\ny:\t'+str(self.speed['y'][-1])+'\nz:\t'+str(self.speed['z'][-1])
+            self.cur_acc = "\tAcceleration: \nx:\t"+ str(self.acc['x'][-1]) + '\ny:\t'+str(self.acc['y'][-1])+'\nz:\t'+str(self.acc['z'][-1])
+            self.cur_acc_ang = "\tAngular Acceleration: \nx:\t"+ str(self.acc_ang['x'][-1]) + '\ny:\t'+str(self.acc_ang['y'][-1])+'\nz:\t'+str(self.acc_ang['z'][-1])
+            self.cur_acc_ang_max = "\tMax Angular Acceleration: \nx:\t"+  str(max(self.acc_ang['x'])) + '\ny:\t'+str(max(self.acc_ang['y']))+'\nz:\t' + str(max(self.acc_ang['z']))
+
+            self.label_frame.remove(self.cur_velo_label)
+            self.cur_velo_label = tk.Label(text=self.cur_velo)
+            self.label_frame.add(self.cur_velo_label)
+            self.label_frame.remove(self.cur_acc_label)
+            self.cur_acc_label = tk.Label(text=self.cur_acc)
+            self.label_frame.add(self.cur_acc_label)
+            self.label_frame.remove(self.cur_acc_ang_label)
+            self.cur_acc_ang_label = tk.Label(text=self.cur_acc_ang)
+            self.label_frame.add(self.cur_acc_ang_label)
+            self.label_frame.remove(self.cur_acc_ang_max_label)
+            self.cur_acc_ang_max_label = tk.Label(text=self.cur_acc_ang_max)
+            self.label_frame.add(self.cur_acc_ang_max)
+
+
             self.canvas.draw()
 
 
@@ -68,6 +108,16 @@ class Application(tk.Frame):
         # This is for the GUI
         self.canvas=FigureCanvasTkAgg(fig,master=root)
         self.canvas.get_tk_widget().grid(row=0,column=0)
+
+        # Starting the Data Column
+        data_fig = plt.figure(figsize=plt.figaspect(2.))
+        data_fig.suptitle('Current Data for Red Sparrow')
+
+        # Plots
+
+        # .....
+        self.label_frame = tk.PanedWindow(orient="vertical")
+        self.label_frame.grid(row=0, column=1) 
         self.canvas.draw()
 
 
